99d2245eedd931d8af12c51570aae36e41047b24
arun <arunkumarshahi@gmail.com>
2019-06-08 20:28:29 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.dao.User;
+import com.example.demo.repo.UserRepository;
+
+@Controller
+public class HomeController {
+	private UserRepository repository;
+    @Autowired
+    public HomeController(UserRepository repository) {
+        this.repository = repository;
+    }
+    @GetMapping("/")
+    public String home(Model model) {
+        List<User> users = repository.findAll();
+        model.addAttribute("users", users);
+        return "home";
+    }
+}
