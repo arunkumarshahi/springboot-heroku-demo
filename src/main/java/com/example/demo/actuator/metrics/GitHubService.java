8a9cc5549b1e42384f0841953463e0680535c99a
arunkumarshahi@gmail.com <arunkumarshahi@gmail.com>
2019-06-10 10:27:37 +0530

    added custom health indicator .

--- src/main/java/com/example/demo/actuator/metrics/GitHubService.java
+++ src/main/java/com/example/demo/actuator/metrics/GitHubService.java
@@ -1,30 +1,25 @@
 package com.example.demo.actuator.metrics;
 
 
+import javax.persistence.PostPersist;
+
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;com.apress.demo.models.GitHubUser;
+import org.springframework.web.client.RestTemplate;
 
-@Configuration
+import com.example.demo.dao.User;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
+
+//@Configuration
 public class GitHubService {
-	@Autowired
-	GaugeService gaugeService;
-
-	public GitHubUser getUserInfo(String username) {
-		RestTemplate restTemplate = new RestTemplate();
-		String url = "https://api.github.com/users/" + username;
-		GitHubUser gitHubUser = null;
-		try {
-			long start = System.currentTimeMillis();
-			gitHubUser = restTemplate.getForObject(url, GitHubUser.class);
-			long end = System.currentTimeMillis();
-			gaugeService.submit("gauge.guthub.response-time", (end - start));
-		} catch (RestClientException e) {
-			e.printStackTrace();
-		}
-		return gitHubUser;
-	}
-}}
+	 @PostPersist
+	    public void handleAfterCreate(User order) {
+	        final Counter counter = Metrics.counter("entity.count", "type", "order");
+	        counter.increment();
+	    }
+	}    
