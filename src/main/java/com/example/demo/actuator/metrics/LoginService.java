9d872091ff40236fdf99943c0eef7f785780492e
arunkumarshahi@gmail.com <arunkumarshahi@gmail.com>
2019-06-10 23:45:28 +0530

    added metrics in actuator

---
+++ src/main/java/com/example/demo/actuator/metrics/LoginService.java
@@ -0,0 +1,23 @@
+package com.example.demo.actuator.metrics;
+
+import org.springframework.stereotype.Service;
+
+import io.micrometer.core.instrument.Metrics;
+
+@Service
+public class LoginService {
+
+	 public boolean login(String email, String password)
+	    {
+	        if("admin@gmail.com".equalsIgnoreCase(email) && "admin".equals(password)){
+	            //counterService.increment("counter.login.success");
+	        	Metrics.counter("counter.login.success").increment(1.0);
+	            return true;
+	        } else {
+	        	Metrics.counter("counter.login.failure").increment(1.0);
+	            //counterService.increment("counter.login.failure");
+	            return false;
+	        }
+	    }
+	}
+}
