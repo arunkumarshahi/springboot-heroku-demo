16dcb3f27e46370871461147e6e936432033fb20
arunkumarshahi@gmail.com <arunkumarshahi@gmail.com>
2019-06-11 08:15:41 +0530

    added MeterRegistry

--- src/main/java/com/example/demo/actuator/metrics/LoginService.java
+++ src/main/java/com/example/demo/actuator/metrics/LoginService.java
@@ -1,17 +1,21 @@
 package com.example.demo.actuator.metrics;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Metrics;
 
 @Service
 public class LoginService {
-
+@Autowired 
+MeterRegistry meterRegistry;
 	 public boolean login(String email, String password)
 	    {
 	        if("admin@gmail.com".equalsIgnoreCase(email) && "admin".equals(password)){
 	            //counterService.increment("counter.login.success");
 	        	Metrics.counter("counter.login.success").increment(1.0);
+	        	meterRegistry.counter("login.access", "accessCounter").increment();
 	            return true;
 	        } else {
 	        	Metrics.counter("counter.login.failure").increment(1.0);
